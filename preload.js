eb726fbd95074b25bb030b36355e1d0fb979d59c
woynert <whayner.porras.2020@upb.edu.co>
2023-08-03 17:27:20 -0500

    feat: Add resizable terminal

--- preload.js
+++ preload.js
@@ -1,14 +1,42 @@
 // preload.js
 
-// All the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+window.addEventListener("DOMContentLoaded", () => {
+  // terminal
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
-})
+  const Terminal = require("xterm").Terminal;
+  const FitAddon = require("xterm-addon-fit").FitAddon;
+  const ipcRenderer = require("electron").ipcRenderer;
+
+  const term = new Terminal({
+    fontFamily: 'Hack Nerd Font Mono',
+    cursorBlink: true,
+    experimentalCharAtlas: "dynamic"
+  });
+  const fitAddon = new FitAddon();
+  const term_container = document.getElementById("terminal-container");
+
+  term.loadAddon(fitAddon);
+  term.open(term_container);
+
+  // ipc signals
+
+  term.onData((e) => {
+    ipcRenderer.send("terminal-into", e);
+  });
+  ipcRenderer.on("terminal-incData", (event, data) => {
+    term.write(data);
+  });
+
+  new ResizeObserver(() => {
+    ipcRenderer.send("terminal-resize", fitAddon.proposeDimensions());
+    fitAddon.fit();
+  }).observe(term_container);
+});
+
+const { contextBridge } = require("electron");
+
+contextBridge.exposeInMainWorld("myAPI", {
+  xterm: require("xterm").Terminal,
+  "xterm-addon-fit": require("xterm-addon-fit").FitAddon,
+  ipcRenderer: require("electron").ipcRenderer,
+});
