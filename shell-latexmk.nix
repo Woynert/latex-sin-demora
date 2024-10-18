78a6c86fbe07f6d165518dd9b5eeb744de5965f4
woynert <woynertgamer@gmail.com>
2024-10-18 09:27:00 -0500

    feat: make nix-shell more readable

--- shell-latexmk.nix
+++ shell-latexmk.nix
@@ -1,31 +1,32 @@
-let
-  pkgs = import (fetchTarball {
-    name = "nixos-23.05";
-	url = "https://github.com/NixOS/nixpkgs/archive/b7cde1c47b73.tar.gz";
-    sha256 = "0xry0farxln7cjas4zfip4s1hfl2x9lbwb0imsla0zqvzhvbz729";
-  }) { };
+# If you prefer to use pinned revision:
+#with import
+#    (builtins.fetchTarball {
+#        name = "nixos-23.05";
+#        url = "https://github.com/NixOS/nixpkgs/archive/b7cde1c47b73.tar.gz";
+#        sha256 = "0xry0farxln7cjas4zfip4s1hfl2x9lbwb0imsla0zqvzhvbz729";
+#    })
+#    { };
 
-in pkgs.mkShell {
+with import <nixpkgs> {};
+
+mkShell {
 	name = "dev-environment";
 	buildInputs = [
-		pkgs.git
-        pkgs.nodejs_20
+		git
+        nodejs_20
         
         # compiling node-pty module
-        pkgs.python39
-        pkgs.gnumake
-        pkgs.gcc
+        python39
+        gnumake
+        gcc
 
         # runtime dependecy
-        pkgs.texlive.combined.scheme-full
+        texlive.combined.scheme-full
 	];
-	shellHook = ''
+    shellHook = ''
 		echo "Starting latex-sin-demora development shell"
-
-		# custom prompt
-		if [ -e ~/.gitconfig ] && [ -f ~/.git-prompt.sh ]; then
-			source ~/.git-prompt.sh
-			PS1='\[\033[01;33m\]nix:\w\[\033[01;34m\]$(__git_ps1 " %s")\[\033[33m\]\$\[\033[00m\] '
-		fi
+		# git prompt
+		source ${git}/share/git/contrib/completion/git-prompt.sh
+		PS1='\[\033[0;33m\]nix:\w\[\033[0m\] $(__git_ps1 %s)\n$ '
 	'';
 }
